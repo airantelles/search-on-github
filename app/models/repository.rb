8b2615136e463a413f1ee3573c164431fe5375c4
Airan <airan.telles@bravosul.com.br>
2021-04-19 01:56:02 -0300

    Created the repository model.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,9 @@
 class Repository < ApplicationRecord
   belongs_to :language
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :language_id, presence: true
+  validates :external_id, presence: true
+  validates :owner_name, presence: true
 end
